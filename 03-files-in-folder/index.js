fdadf5a9ee3be9a2734ba3f2ffff4be3de08a79a
Tatsiana Zatonets <koshka.tan@gmail.com>
2021-11-09 00:47:53 +0300

    feat: add checking error

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,9 +4,11 @@ const path = require('path');
 const dirPath = path.join(__dirname, 'secret-folder');
 
 readdir(dirPath, ['utf8', 'true'], (err, files) => {
+    if (err) throw err;
     for (const file of files) {
         const filePath = path.join(dirPath, file);
         stat(filePath, (err, stats) => {
+            if (err) throw err;
             if (stats.isDirectory() === false) {
                 console.log(`${path.parse(filePath).name} - ${path.extname(filePath).slice(1)} - ${stats.size / 1000}kb`)
             }
